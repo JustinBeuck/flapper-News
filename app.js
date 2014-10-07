aef66ad1631a8b779043226320f6ff9c27d73864
Justin Beuck <jbeuck@yahoo.com>
2014-10-07 13:25:42 -0500

    completed Angular section of course

--- app.js
+++ app.js
@@ -8,10 +8,16 @@ angular.module('flapperNews', ['ui.router'])
 				url: '/home',
 				templateUrl: '/home.html',
 				controller: 'MainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
 			});
 
 			$urlRouterProvider.otherwise('home');
 	}])
+
 .factory('posts', [function(){
 	var o = {
 		posts: []
@@ -19,28 +25,45 @@ angular.module('flapperNews', ['ui.router'])
 	return o;
 }])
 
+.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+	}])
+
 .controller('MainCtrl', [
 	'$scope',
 	'posts',
 	function($scope, posts){
 		$scope.posts = posts.posts;
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 15},
-			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}		
-			];
+
 	$scope.addPost = function(){
 		if($scope.title === '') { return; }
 		$scope.posts.push({
 			title: $scope.title,
 			link: $scope.link,
-			upvotes: 0, upvotes:0
+			upvotes: 0,
+			comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  		] 
 		});
 		$scope.title = '';
 		$scope.link = '';
 		};
+
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
 		};
