4569b21796cb4efa172d24a4159a6df4ec28e98e
Justin Beuck <jbeuck@yahoo.com>
2014-10-09 18:56:10 -0500

    completed tutorial

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -18,6 +18,11 @@ angular.module('flapperNews', ['ui.router'])
 				url: '/posts/{id}',
 				templateUrl: '/posts.html',
 				controller: 'PostsCtrl',
+				resolve: {
+  			post: ['$stateParams', 'posts', function($stateParams, posts) {
+    			return posts.get($stateParams.id);
+  }]
+}
 			});
 
 			$urlRouterProvider.otherwise('home');
@@ -31,32 +36,51 @@ angular.module('flapperNews', ['ui.router'])
 	  o.getAll = function() {
 	    return $http.get('/posts').success(function(data){
 	      angular.copy(data, o.posts);
-    });
-	  };
+   });
+};
 
 	  o.create = function(post) {
   return $http.post('/posts', post).success(function(data){
     o.posts.push(data);
-  	});
-  };
+  });
+};
+  	o.get = function(id) {
+  return $http.get('/posts/' + id).then(function(res){
+    return res.data;
+  });
+};
+		o.upvoteComment = function(post, comment) {
+  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+    .success(function(data){
+      comment.upvotes += 1;
+    });
+};
+		o.addComment = function(id, comment) {
+  return $http.post('/posts/' + id + '/comments', comment);
+};
   return o;
 }])
 
 .controller('PostsCtrl', [
 	'$scope',
-	'$stateParams',
-	'posts',
-	function($scope, $stateParams, posts){
-		$scope.post = posts.posts[$stateParams.id];
+	'$posts',
+	'post',
+	function($scope, posts, post){
+		$scope.post = post;
 
 		$scope.addComment = function(){
 		  if($scope.body === '') { return; }
-		  $scope.post.comments.push({
+		  posts.addComment(post._id, {
 		    body: $scope.body,
 		    author: 'user',
-		    upvotes: 0
+		  }).success(function(comment) {
+		    $scope.post.comments.push(comment);
 		  });
 		  $scope.body = '';
+		};
+
+		$scope.incrementUpvotes = function(comment){
+  		posts.upvoteComment(post, comment);
 		};
 	}])
 
@@ -77,7 +101,7 @@ angular.module('flapperNews', ['ui.router'])
 	};
 
 	$scope.incrementUpvotes = function(post) {
-		post.upvotes += 1;
+		posts.upvote(post);
 		};
 	}
 ]);
